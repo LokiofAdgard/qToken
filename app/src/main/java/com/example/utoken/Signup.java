0e00d9e42b34c6afb050529e1215f89db3e91009
Dulnath Vanderbona <dulnathvanderbona@gmail.com>
2022-07-23 17:27:41 +0530

    sign ups download arrays

--- app/src/main/java/com/example/utoken/Signup.java
+++ app/src/main/java/com/example/utoken/Signup.java
@@ -5,31 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Signup extends AppCompatActivity {
 
     //String[] NICs = {"111111111111", "1111111111111", "c"};
-    ArrayList<String> NICs = new ArrayList<String>();
+    ArrayList<String> NICs = new ArrayList<>();
     //String[] VIDs = {"111111111111", "1111111111111", "c"};
-    ArrayList<String> VIDs = new ArrayList<String>();
+    ArrayList<String> VIDs = new ArrayList<>();
 
     DatabaseReference databaseUser;
 
@@ -43,11 +37,12 @@ public class Signup extends AppCompatActivity {
         List<User> userList = new ArrayList<>();
         databaseUser.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot snapshot) {
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
                 userList.clear();
                 for (DataSnapshot postSnapshot: snapshot.getChildren()) {
                     User user = postSnapshot.getValue(User.class);
                     userList.add(user);
+                    assert user != null;
                     NICs.add(user.nic);
                     VIDs.add(user.vid);
                     //String name = userList.get(0).getId();
@@ -59,70 +54,62 @@ public class Signup extends AppCompatActivity {
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
                 System.out.println("The read failed: ");
             }
         });
 
-        Button Login = (Button) findViewById(R.id.btn_login);
-        EditText username = (EditText) findViewById(R.id.username);
-        EditText vnoL = (EditText) findViewById(R.id.vnoL);
-        EditText vnoN = (EditText) findViewById(R.id.vnoN);
-        EditText password = (EditText) findViewById(R.id.password);
-        EditText password2 = (EditText) findViewById(R.id.password2);
+        Button Login = findViewById(R.id.btn_login);
+        EditText username = findViewById(R.id.username);
+        EditText vnoL = findViewById(R.id.vnoL);
+        EditText vnoN = findViewById(R.id.vnoN);
+        EditText password = findViewById(R.id.password);
+        EditText password2 = findViewById(R.id.password2);
 
-        Login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String nic = username.getText().toString();
-                String v1 = vnoL.getText().toString();
-                String v2 = vnoN.getText().toString();
-                String pw = password.getText().toString();
-                String pw2 = password2.getText().toString();
+        Login.setOnClickListener(view -> {
+            String nic = username.getText().toString();
+            String v1 = vnoL.getText().toString();
+            String v2 = vnoN.getText().toString();
+            String pw = password.getText().toString();
+            String pw2 = password2.getText().toString();
 
-                if(nic.equals("") || pw.equals("") || pw2.equals("") || v1.equals("") || v2.equals("")){
-                    Toast.makeText(Signup.this, "Please Fill All Fields", Toast.LENGTH_SHORT).show();
-                }
-                else if (nic.length()!=12 && nic.length()!=13) {
-                    Toast.makeText(Signup.this, "Invalid NIC", Toast.LENGTH_SHORT).show();
-                }
-                else if (NICs.contains(nic)) {
-                    Toast.makeText(Signup.this, "NIC already registered", Toast.LENGTH_SHORT).show();
-                }
-                else if (v1.length()>3) {
-                    Toast.makeText(Signup.this, "Invalid Vehicle details", Toast.LENGTH_SHORT).show();
-                }
-                else if (v2.length()!=4) {
-                    Toast.makeText(Signup.this, "Invalid Vehicle details", Toast.LENGTH_SHORT).show();
-                }
-                else if (VIDs.contains(v1+v2)) {
-                    Toast.makeText(Signup.this, "Vehicle ID already registered", Toast.LENGTH_SHORT).show();
-                }
-                else if (pw.length()<5) {
-                    Toast.makeText(Signup.this, "Password too short", Toast.LENGTH_SHORT).show();
-                }
-                else if (!(pw.equals(pw2))) {
-                    Toast.makeText(Signup.this, "Passwords do not Match", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    //TODO: add entry
+            if(nic.equals("") || pw.equals("") || pw2.equals("") || v1.equals("") || v2.equals("")){
+                Toast.makeText(Signup.this, "Please Fill All Fields", Toast.LENGTH_SHORT).show();
+            }
+            else if (nic.length()!=12 && nic.length()!=13) {
+                Toast.makeText(Signup.this, "Invalid NIC", Toast.LENGTH_SHORT).show();
+            }
+            else if (NICs.contains(nic)) {
+                Toast.makeText(Signup.this, "NIC already registered", Toast.LENGTH_SHORT).show();
+            }
+            else if (v1.length()>3) {
+                Toast.makeText(Signup.this, "Invalid Vehicle details", Toast.LENGTH_SHORT).show();
+            }
+            else if (v2.length()!=4) {
+                Toast.makeText(Signup.this, "Invalid Vehicle details", Toast.LENGTH_SHORT).show();
+            }
+            else if (VIDs.contains(v1+v2)) {
+                Toast.makeText(Signup.this, "Vehicle ID already registered", Toast.LENGTH_SHORT).show();
+            }
+            else if (pw.length()<5) {
+                Toast.makeText(Signup.this, "Password too short", Toast.LENGTH_SHORT).show();
+            }
+            else if (!(pw.equals(pw2))) {
+                Toast.makeText(Signup.this, "Passwords do not Match", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                //TODO: add entry
 
-                    String id = databaseUser.push().getKey();
-                    User user = new User(id, nic, (v1+v2), pw);
-                    assert id != null;
-                    databaseUser.child(id).setValue(user);
-                    Toast.makeText(Signup.this, "Successfully Registered", Toast.LENGTH_SHORT).show();
+                String id = databaseUser.push().getKey();
+                User user = new User(id, nic, (v1+v2), pw);
+                assert id != null;
+                databaseUser.child(id).setValue(user);
+                Toast.makeText(Signup.this, "Successfully Registered", Toast.LENGTH_SHORT).show();
 
 
-                    //Intent myIntent = new Intent(Signup.this, User_Login.class);
-                    //Signup.this.startActivity(myIntent);
-
-                }
-
-
+                //Intent myIntent = new Intent(Signup.this, User_Login.class);
+                //Signup.this.startActivity(myIntent);
             }
         });
-
-
     }
 }
